fff664ca1458a422479855c4a09b2b9a92ca93aa
Karl Alnebratt <karl.alnebratt@gmail.com>
2020-04-21 13:48:53 +0200

    Adds ApolloProvider  - Adds apollo client to context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+import {
+  ApolloProvider,
+} from '@apollo/client'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Client from './Client'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={Client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
