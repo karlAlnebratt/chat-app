0eacc3bc804aa202372b59c0a3cd1765049e9009
Karl Alnebratt <karl.alnebratt@gmail.com>
2020-04-22 13:10:25 +0200

    Adds simple error handling  - Logs error to console

--- src/Client.js
+++ src/Client.js
@@ -1,10 +1,38 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, gql, HttpLink, ApolloLink, InMemoryCache } from '@apollo/client';
+import { onError } from "@apollo/link-error";
+
+import {
+  IS_LOGGED_IN_QUERY,
+} from './Schema'
+
+const cache = new InMemoryCache()
+
+cache.writeQuery({
+  query: IS_LOGGED_IN_QUERY,
+  data: {
+    isLoggedIn: !!localStorage.getItem('userId')
+  },
+});
 
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:4000',
-  })
+  cache,
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError, response }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+
+    }),
+    new HttpLink({
+      uri: 'http://192.168.1.206:4000',
+    })
+  ]),
 });
+
 
 export default client
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import {
-  ApolloProvider,
-} from '@apollo/client'
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css'
+import { ErrorBoundary } from 'react-error-boundary';
+
+import 'normalize.css';
 import './index.css';
 
 import App from './App';
-import Client from './Client'
+import Client from './Client';
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={Client}>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
+)
